2b19d3f610ffeb628c1fd49573a8a192fb3fca6c
LAPTOP-LV05Q557\Peter <peter.yudhistiraa@gmail.com>
2024-08-02 10:24:32 +0700

    [Aug 2 2024] initial Spring Boot commit

---
+++ src/main/java/kafka/project/recommender_system/RecommenderSystemApplication.java
@@ -0,0 +1,13 @@
+package kafka.project.recommender_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kafka/project/recommender_system/RecommenderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka.project.recommender_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
